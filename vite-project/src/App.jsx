1b606b9cae309bedc3a1535c03fd33a0320fef90
Abh03 <abhyu.adhikari@gmail.com>
2024-10-24 13:14:51 +0545

    initial set up

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='bg-red-600'>KUdosss  <Button>Click me</Button></div>
+  )
+}
