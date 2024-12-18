ba4fc6f93433d873f9f0f4db4b22c1455d640aba
karib3 <karibmaharjan@gmail.com>
2024-12-18 17:38:16 +0545

    incomplete versus card

---
+++ vite-project/src/components/versuscard/VersusCard.jsx
@@ -0,0 +1,18 @@
+const jerseys =[
+    {src:"https://i.etsystatic.com/15536434/r/il/1c23d2/3645992136/il_1588xN.3645992136_ot1u.jpg",
+     alt:"Image of red jersey"
+    },
+    {src:"https://i.etsystatic.com/15536434/r/il/6784b4/3646387848/il_1588xN.3646387848_d8xu.jpg",
+     alt:"Image of blue jersey"
+    }
+]
+
+const VersusCard =() => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default VersusCard
