d43f1e4417e58698166355b8831a4cbb05af8661
Saumya <Saumya@gmail.com>
2024-12-21 23:01:18 +0545

    football

--- vite-project/src/pages/football/Football.jsx
+++ vite-project/src/pages/football/Football.jsx
@@ -5,7 +5,7 @@ const Football = () => {
     <Layout>
       <div className="flex justify-center items-center py-10">
         {/* Scorecard Container */}
-        <div className="bg-[#bfbfbf] shadow-md rounded-lg border border-[#387478] p-8 w-[90%] max-w-4xl">
+        <div className=" shadow-md rounded-lg border border-[#387478] p-8 w-[90%] max-w-4xl">
           <div className="flex justify-between items-center px-10">
             {/* Left Team */}
             <div className="text-center space-y-4">
@@ -14,8 +14,8 @@ const Football = () => {
                 alt="Team A Jersey"
                 className="w-20 mx-auto"
               />
-              <h3 className="font-semibold text-xl text-white">Man City</h3>
-              <div className="text-sm text-white space-y-2">
+              <h3 className="font-semibold text-xl text-[#387478]">Man City</h3>
+              <div className="text-sm text-[#387478] space-y-2">
                 <p>P. Foden 22'</p>
                 <p>K. DeBruyne 66'</p>
               </div>
@@ -37,21 +37,21 @@ const Football = () => {
                 alt="Team B Jersey"
                 className="w-20 mx-auto"
               />
-              <h3 className="font-semibold text-xl text-white">Man City</h3>
-              <div className="text-sm text-white space-y-2">
+              <h3 className="font-semibold text-xl text-[#387478]">Man City</h3>
+              <div className="text-sm text-[#387478] space-y-2">
                 <p>E. Haaland 55'</p>
                 <p>B. Silva 84'</p>
               </div>
             </div>
           </div>
           <div className="flex justify-center gap-20 mt-4 mb-0">
-            <button className="text-white hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
+            <button className="text-[#387478] hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
               Lineup
             </button>
-            <button className="text-white hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
+            <button className="text-[#387478] hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
               Result
             </button>
-            <button className="text-white hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
+            <button className="text-[#387478] hover:text-[#387478] text-lg font-semibold border-b-2 border-transparent hover:border-[#387478]">
               Table
             </button>
           </div>
