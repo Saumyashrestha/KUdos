a09cb2917110882399ec7b1fd2e990eaec7a95fc
Abh03 <abhyu.adhikari@gmail.com>
2025-01-23 16:29:52 +0545

    matches from new matches collecotijno

--- vite-project/src/pages/clubhome/ClubHome.jsx
+++ vite-project/src/pages/clubhome/ClubHome.jsx
@@ -2,12 +2,10 @@ import SportsCategory from "../../components/sportscategory/SportsCategory";
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/heroSection"
 import HomePageCard from "../../components/homePageCard/HomePageCard";
-import { useLocation } from "react-router-dom";
+
 
 
 const ClubHome = () => {
-    const location = useLocation();
-    const { club } = location.state || {};
     return(
         <Layout>
             <HeroSection/>
