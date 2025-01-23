21930cbfdef5cebafc83e93557ab46c6eb0a6948
Abh03 <abhyu.adhikari@gmail.com>
2025-01-23 13:39:48 +0545

    sgksbdjbgsrg

--- vite-project/src/pages/clubhome/ClubHome.jsx
+++ vite-project/src/pages/clubhome/ClubHome.jsx
@@ -2,9 +2,12 @@ import SportsCategory from "../../components/sportscategory/SportsCategory";
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/heroSection"
 import HomePageCard from "../../components/homePageCard/HomePageCard";
+import { useLocation } from "react-router-dom";
 
 
 const ClubHome = () => {
+    const location = useLocation();
+    const { club } = location.state || {};
     return(
         <Layout>
             <HeroSection/>
